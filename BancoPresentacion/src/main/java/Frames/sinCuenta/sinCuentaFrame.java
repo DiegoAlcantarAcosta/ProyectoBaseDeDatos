812e167eeca2bba1779dd5e619b49b8a389f246c
DiegoAlcantarAcosta <haloalcantar@gmail.com>
2024-02-14 13:38:14 -0700

    Revert "boton aceptar sinCuentaFrame"
    This reverts commit 20ab632725deda24088930e4ff95089a34284eec.

--- BancoPresentacion/src/main/java/Frames/sinCuenta/sinCuentaFrame.java
+++ BancoPresentacion/src/main/java/Frames/sinCuenta/sinCuentaFrame.java
@@ -58,6 +58,8 @@ public class sinCuentaFrame extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel4.setText("Monto:");
 
+        montoTextField.setEditable(false);
+
         aceptarButton.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         aceptarButton.setText("Aceptar");
         aceptarButton.addActionListener(new java.awt.event.ActionListener() {
