44226e026934ebafbb1662d4d75ff0a03b3663cf
DiegoAlcantarAcosta <haloalcantar@gmail.com>
2024-02-14 13:42:03 -0700

    Reapply "boton aceptar sinCuentaFrame"
    This reverts commit 812e167eeca2bba1779dd5e619b49b8a389f246c.

--- BancoPresentacion/src/main/java/Encriptador/Encriptador.java
+++ BancoPresentacion/src/main/java/Encriptador/Encriptador.java
@@ -4,10 +4,54 @@
  */
 package Encriptador;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 /**
  *
  * @author lv1821
  */
 public class Encriptador {
-    
-}
+    public static void main(String[] args) throws Exception {
+        // Generar una clave secreta
+        SecretKey secretKey = generateSecretKey();
+
+        // Texto original
+        String originalText = "Hola, mundo!";
+
+        // Cifrar el texto original
+        String encryptedText = encrypt(originalText, secretKey);
+        System.out.println("Texto cifrado: " + encryptedText);
+
+        // Desencriptar el texto cifrado
+        String decryptedText = decrypt(encryptedText, secretKey);
+        System.out.println("Texto desencriptado: " + decryptedText);
+    }
+
+    private static SecretKey generateSecretKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        return keyGenerator.generateKey();
+    }
+
+    private static String encrypt(String text, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    private static String decrypt(String encryptedText, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes);
+    }
+    }
+
--- BancoPresentacion/src/main/java/Frames/test.java
+++ BancoPresentacion/src/main/java/Frames/test.java
@@ -16,7 +16,7 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      IniciarFrame i = new IniciarFrame();
+      RegistrarFrame i = new RegistrarFrame();
               i.show();
     }
     
