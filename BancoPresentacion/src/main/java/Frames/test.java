d9e2ec90821d44a38a8be2cf68d08e7d1d971d89
Madero244903 <159743718+Madero244903@users.noreply.github.com>
2024-02-14 13:46:48 -0700

    main

--- BancoPresentacion/src/main/java/Frames/test.java
+++ BancoPresentacion/src/main/java/Frames/test.java
@@ -16,7 +16,7 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      RegistrarFrame i = new RegistrarFrame();
+      IniciarFrame i = new IniciarFrame();
               i.show();
     }
     
